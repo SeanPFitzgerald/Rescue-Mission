795a3b7c0a83aaa4135c98e0a9652a8b46bd9812
Sean Fitzgerald <seanfitzgerald@Seans-MacBook.local>
2018-01-15 15:25:51 -0500

    view all questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(username: "jonsnow", provider: "github")
+u2 = User.create(username: "nedstark", provider: "facebook")
+u3 = User.create(username: "jaimelannister", provider: "twitter")
+
+q1 = Question.create(title: "Are you sure that this is cool?",
+                  description: "Checking if this should be at least fifty characters",
+                  user: u1)
+
+q1 = Question.create(title: "Are you sure that this is dumb?",
+                  description: "Making sure that this is at least fifty characters though",
+                  user: u1)
+
+q1 = Question.create(title: "Are you sure that this is rad?",
+                  description: "Could all this be rad as long as there is at least fifty characters",
+                  user: u2)
+
+q1 = Question.create(title: "Are you sure that this is baller?",
+                  description: "Can anything be baller if it is at least fifty characters",
+                  user: u3)
